85483c25696555ff918fff29e2a74e251654d63f
Regiane Gaspar <123254220+regianegaspar@users.noreply.github.com>
2023-05-20 11:30:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+/*
+Posso fazer um para cada som:
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+ou simplesmente
+document.querySelectorAll('.tecla');*/
+
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function(evento){
+        if(evento.code == 'Space' || evento.code == 'Enter'){
+        tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+}
